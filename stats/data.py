e5b8d6e8dcbcbcf88ae8428e21b2f9bd97287a25
byohannes <42332389+byohannes@users.noreply.github.com>
2019-03-31 14:33:00 +0100

    data updated

--- stats/data.py
+++ stats/data.py
@@ -20,7 +20,7 @@ identifiers.columns = ['game_id', 'year']
 
 games = pd.concat ([games, identifiers], axis=1, sort=False)
 
-games = games.fillna ('')
+games = games.fillna (' ')
 
 games.loc[:, 'type'] = pd.categorical (games.loc[:, 'type'])
 
